0a23f7064045f9ad90522b647fdc357addb7f477
raquelbrandino <raquelbrandino@hotmail.com>
2019-07-26 11:54:17 -0300

    fazer sistema de cadastro e adicionar class conta

--- contas/views.py
+++ contas/views.py
@@ -3,13 +3,17 @@ from .models import Pessoa
 
 # Create your views here.
 def mostrar_formulario_cadastro(request):
-    if request.method == 'POST':       
+    contexto = {'msg': None}
+    if request.method == 'POST':
         pessoa = Pessoa()
-        pessoa.nome = request.POST['nome']
-        pessoa.cpf = request.POST['cpf']
+        pessoa.nome = request.POST.get('nome')
+        pessoa.cpf = request.POST.get('cpf')
+        pessoa.email = request.POST.get('email')
+        pessoa.telefone = request.POST.get('telefone')
+        pessoa.genero = request.POST.get('genero')
         pessoa.save()
-
-    return render(request, 'index.html')
+        contexto = {'msg': 'ae parabéns'}
+    return render(request, 'index.html', contexto)
 
 def mostrar_pessoas(request):
     pessoas = Pessoa.objects.all()
